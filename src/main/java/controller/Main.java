7636eddb89b76edd431ecc8685f7ee5d3c1668e7
Heorhii <grishasht@bigmir.net>
2019-07-07 17:33:00 +0300

    Documentary and interface added

--- src/main/java/controller/Main.java
+++ src/main/java/controller/Main.java
@@ -1,10 +1,11 @@
 package controller;
 
 import model.HashMap;
+import model.Map;
 
 public class Main {
     public static void main(String[] args) {
-        HashMap map = new HashMap(128);
+        Map map = new HashMap(128);
 
         for (int i = 0; i < 65; i++){
             map.put(i*i, i);
---
+++ src/main/java/model/Map.java
@@ -0,0 +1,9 @@
+package model;
+
+public interface Map {
+    void put(int key, long value);
+
+    Long get(int key);
+
+    int size();
+}
