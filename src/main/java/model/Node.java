c0f1821033651e618c30e5a2ceca6df01bbcf67d
Heorhii <grishasht@bigmir.net>
2019-07-07 19:51:20 +0300

    get() and size() tests added

--- src/main/java/model/Node.java
+++ src/main/java/model/Node.java
@@ -1,33 +1,26 @@
 package model;
 
-public class Node {
+class Node {
     private int hash;
     private int key;
     private long value;
 
-    public Node(int hash, int key, long value) {
+    Node(int hash, int key, long value) {
         this.hash = hash;
         this.key = key;
         this.value = value;
     }
 
 
-    public int getHash() {
+    int getHash() {
         return hash;
     }
 
-    public int getKey() {
+    int getKey() {
         return key;
     }
 
-    public long getValue() {
+    long getValue() {
         return value;
-    }
-
-    @Override
-    public String toString() {
-        return "hash=" + hash +
-                "key=" + key +
-                "value=" + value + "\n";
     }
 }
